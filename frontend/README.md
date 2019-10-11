b7cf517edbd124ccc2c0c628309bf3bb41f3d93e
Anselmo Pfeifer <anspfeifer@gmail.com>
2019-10-11 22:32:53 +0300

    Added service to connect on external api

--- frontend/README.md
+++ frontend/README.md
@@ -1,1 +1,5 @@
-# FrontEnd 
+# Learning React Native (FrontEnd)
+
+- The proposed of this is to create a frontend using React
+- `yarn create react-app frontend`
+- `yarn add dotenv axios`
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import dotenv from 'dotenv'
+
+import Api from './services/api'
 import './App.css';
 import logo from './assets/logo.svg';
+// dotenv.config();
 
 function App() {
+  const [ email, setEmail ] = useState('')
+  async function handleSubmit(event) {
+    event.preventDefault() 
+      const response = await Api.post('/sessions', { email })
+      console.log(response)
+    }
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
       <div className="content">
         <p>
-          Oferece <strong>spots</strong> 
-          para programadores e encontre <strong>talentos para sua enpresa!</strong>
+          Oferece <strong>spots</strong> para programadores e encontre <strong>talentos </strong> para sua enpresa!
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail</label>
           <input 
             id="email"
             type="email"
-            placeholder="email"
+            placeholder="Seu melhor e-mail"
+            onChange={event => setEmail(event.target.value)}
+            required
           />
 
           <button className="login" type="submit">Login</button>
